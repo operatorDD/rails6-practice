6ae0d02260bb11633770f4d387c4526a38ae415f
operatorDD <enqhrdk@gmail.com>
2020-05-16 00:58:57 +0900

    [clean]例外処理をモジュール化 ErrorHandlers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,10 +4,7 @@ class ApplicationController < ActionController::Base
   class Forbidden < ActionController::ActionControllerError; end
   class IpAddressRejected < ActionController::ActionControllerError; end
 
-  rescue_from StandardError, with: :rescue_500
-  rescue_from Forbidden, with: :rescue_403
-  rescue_from IpAddressRejected, with: :rescue_403
-  rescue_from ActiveRecord::RecordNotFound, with: :rescue_404
+  include ErrorHandlers
 
   private
 
@@ -17,19 +14,5 @@ class ApplicationController < ActionController::Base
     else
       'customer'
     end
-  end
-
-  def rescue_403(e)
-    @exception = e
-    render 'errors/forbidden', status: 403
-  end
-
-  def rescue_404(e)
-    @exception = e
-    render 'errors/not_found_record'
-  end
-
-  def rescue_500(e)
-    render 'errors/internal_server_error', status: 500
   end
 end
