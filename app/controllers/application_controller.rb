d8af569a516750107a900d54e014835c5ea72cda
operatorDD <enqhrdk@gmail.com>
2020-05-15 23:21:34 +0900

    [add]500エラーページ追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,19 @@
 class ApplicationController < ActionController::Base
   layout :set_layout
 
-  private def set_layout
+  rescue_from StandardError, with: :rescue_500
+
+  private
+
+  def set_layout
     if params[:controller].match(%r{\A(staff|admin|customer)/})
       Regexp.last_match[1]
     else
       'customer'
     end
+  end
+
+  def rescue_500(e)
+    render 'errors/internal_server_error', status: 500
   end
 end
