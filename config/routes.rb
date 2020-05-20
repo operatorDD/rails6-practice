d7a95e1422345f57cd0656f0623def5608687407
operatorDD <enqhrdk@gmail.com>
2020-05-21 00:00:25 +0900

    [add]staffログインページ追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
-  devise_for :staff
+  # devise_for :staff
+  devise_for :staff, path: 'staffs', controllers: { sessions: 'staff/sessions' }
   devise_for :admin
 
   namespace :staff do
     root 'top#index'
+    
   end
 
   namespace :admin do
