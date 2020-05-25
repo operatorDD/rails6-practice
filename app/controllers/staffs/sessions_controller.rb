e0cf597b81f71a9b79b485e0f4ad007289b7e073
operatorDD <enqhrdk@gmail.com>
2020-05-25 21:29:34 +0900

    [update]ログアウト処理後に移動するページ設定

--- app/controllers/staffs/sessions_controller.rb
+++ app/controllers/staffs/sessions_controller.rb
@@ -1,2 +1,8 @@
 class Staffs::SessionsController < Devise::SessionsController
+  def destroy
+    signed_out = (Devise.sign_out_all_scopes ? sign_out : sign_out(resource_name))
+    set_flash_message! :notice, :signed_out if signed_out
+    yield if block_given?
+    redirect_to :staffs_root
+  end
 end
