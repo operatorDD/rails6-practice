566ba2f62cda096894f2569a5478912c687b2be0
operatorDD <enqhrdk@gmail.com>
2020-05-28 23:57:34 +0900

    [update]ホスト名による制約追加 ホスト名：baukis2.example.com

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,20 @@
 Rails.application.routes.draw do
+  config = Rails.application.config.baukis2
   devise_for :staffs, path: 'staffs', controllers: { sessions: 'staffs/sessions' }
   devise_for :admins, path: 'admins', controllers: { sessions: 'admins/sessions' }
 
-  namespace :staffs do
-    root 'top#index'
-    resource :account, except: %i[new create destroy]
+  constraints host: config[:staffs][:host] do
+    namespace :staffs, path: config[:staffs][:path] do
+      root 'top#index'
+      resource :account, except: %i[new create destroy]
+    end
   end
 
-  namespace :admins do
-    root 'top#index'
-    resources :staffs
+  constraints host: config[:admins][:host] do
+    namespace :admins, path: config[:admins][:path] do
+      root 'top#index'
+      resources :staffs
+    end
   end
 
   namespace :customers do
