5aaab8cc6c86d2ded63d6393e2d0ce31305d3b2f
operatorDD <enqhrdk@gmail.com>
2020-05-30 23:56:17 +0900

    [add]顧客テーブル・モデル追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_19_153847) do
+ActiveRecord::Schema.define(version: 2020_05_30_145228) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,22 @@ ActiveRecord::Schema.define(version: 2020_05_19_153847) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["email"], name: "index_admins_on_email", unique: true
     t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
+  end
+
+  create_table "customers", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "family_name", null: false, comment: "姓"
+    t.string "given_name", null: false, comment: "名"
+    t.string "family_name_kana", null: false, comment: "姓（カナ）"
+    t.string "given_name_kana", null: false, comment: "名（カナ）"
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_customers_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_customers_on_reset_password_token", unique: true
   end
 
   create_table "staffs", force: :cascade do |t|
