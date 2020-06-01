386dd3389447eaa68b2f9317ceeb10f0617fee54
duho Kim <operatordd.kdh@gmail.com>
2020-06-01 23:53:48 +0900

    [update]顧客ログイン・ログアウト機能見直し

--- config/initializers/baukis2.rb
+++ config/initializers/baukis2.rb
@@ -2,6 +2,6 @@ Rails.application.configure do
   config.baukis2 = {
     staffs: { host: 'baukis2.example.com', path: '' },
     admins: { host: 'baukis2.example.com', path: 'admins'},
-    customers: { host: 'example.com', path: 'mypage' }
+    customers: { host: 'example.com', path: '' }
   }
 end
