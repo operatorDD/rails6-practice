ca6979496fc220708a78fa426447cc194d539828
duho Kim <operatordd.kdh@gmail.com>
2020-06-16 23:43:04 +0900

    [add]職員登録機能追加

--- app/controllers/admins/staffs_controller.rb
+++ app/controllers/admins/staffs_controller.rb
@@ -1,5 +1,32 @@
 class Admins::StaffsController < ApplicationController
+  before_action :set_staff, only: %i[show edit]
+
   def index
     @staffs = Staff.order(:family_name_kana, :given_name_kana)
+  end
+
+  def show
+    redirect_to [ :edit, :admins, @staff ]
+  end
+
+  def new
+    @staff = Staff.new
+  end
+
+  def edit; end
+
+  def create
+    @staff = Staff.new(params[:staff])
+    if @staff.save
+      redirect_to :admins_staffs
+    else
+      render action: "new"
+    end
+  end
+
+  private
+
+  def set_staff
+    @staff = Staff.find_by(id: params[:id])
   end
 end
