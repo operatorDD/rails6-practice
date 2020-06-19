7b29deb17d631cd1f34b735e5bccc16b7afd5154
duho Kim <operatordd.kdh@gmail.com>
2020-06-19 23:52:04 +0900

    [update]staff画面のアクセス制御修正

--- app/controllers/staffs/base_controller.rb
+++ app/controllers/staffs/base_controller.rb
@@ -1,5 +1,6 @@
 class Staffs::BaseController < ApplicationController
   before_action :staff_user?
+  before_action :check_invalid_acount
 
   private
 
@@ -10,11 +11,17 @@ class Staffs::BaseController < ApplicationController
     redirect_to :new_staff_session
   end
 
-  def active_staff?
-    if current_staff.start_date > Date.today || current_staff.end_date < Date.today || current_staff.suspended
-      false
-    else
-      true
-    end
+  def check_invalid_acount
+    return unless current_staff && invalid_staff?
+
+    session.delete(:staff_id)
+    flash[:alert] = 'アカウントが無効になりました。'
+    redirect_to :staffs_root
+  end
+
+  def invalid_staff?
+    start_date = current_staff.start_date
+    end_date = current_staff.end_date
+    start_date >= Date.today && end_date < Date.today
   end
 end
