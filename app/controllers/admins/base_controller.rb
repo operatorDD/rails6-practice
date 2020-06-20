41d3870cb9d8f5c7a696f1256bed3063ea7840cd
duho Kim <operatordd.kdh@gmail.com>
2020-06-20 23:59:23 +0900

    [add]管理者アカウントの無効確認処理追加

--- app/controllers/admins/base_controller.rb
+++ app/controllers/admins/base_controller.rb
@@ -1,10 +1,21 @@
 class Admins::BaseController < ApplicationController
   before_action :admin_user?
+  before_action :check_invalid_acount
+
+  private
 
   def admin_user?
     return unless current_admin.nil?
 
     flash[:alert] = '管理者としてログインしてください'
     redirect_to :new_admin_session
+  end
+
+  def check_invalid_acount
+    return unless current_admin && current_admin&.suspended?
+
+    sign_out
+    flash[:alert] = 'アカウントが無効になりました。'
+    redirect_to :admins_root
   end
 end
