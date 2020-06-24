35cccd71123f53a574d682af756f81c2cf47d842
duho Kim <operatordd.kdh@gmail.com>
2020-06-25 00:23:02 +0900

    [update]StaffEventHistoriesのindexリファクタリング n+1問題対応、重複処理削除

--- app/controllers/admins/staff_event_histories_controller.rb
+++ app/controllers/admins/staff_event_histories_controller.rb
@@ -2,10 +2,11 @@ class Admins::StaffEventHistoriesController < Admins::BaseController
   def index
     if params[:staff_id]
       @staff = Staff.find(params[:staff_id])
-      @event_histories = @staff.staff_event_histories.order_created_desc
+      event_histories = @staff.staff_event_histories
     else
-      @event_histories = StaffEventHistory.order_created_desc
+      event_histories = StaffEventHistory.all
     end
-    @event_histories = @event_histories.page(params[:page])
+    @event_histories = event_histories.includes(:staff).order_created_desc
+                                      .page(params[:page])
   end
 end
