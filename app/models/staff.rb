d565b7ca21de45b4c483d1cb64391c812eb67da4
duho Kim <operatordd.kdh@gmail.com>
2020-06-26 23:49:07 +0900

    [update]職員登録のバリデーション追加、バリデーション用のcss追加

--- app/models/staff.rb
+++ app/models/staff.rb
@@ -5,4 +5,10 @@ class Staff < ApplicationRecord
          :recoverable, :rememberable, :validatable, :timeoutable
 
   has_many :staff_event_histories, dependent: :destroy
+
+  KATAKANA_REGEXP = /\A[\p{katakana}\u{30fc}]+\z/
+
+  validates :family_name, :given_name, presence: true
+  validates :family_name_kana, :given_name_kana, presence: true,
+    format: { with: KATAKANA_REGEXP, allow_blank: true }
 end
