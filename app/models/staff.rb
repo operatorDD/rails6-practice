a8cd7debf3242e15b7fb198ad28ab72738ccaa31
duho Kim <operatordd.kdh@gmail.com>
2020-06-27 23:53:19 +0900

    [add]氏名とフリガナの正規化バリデーション追加

--- app/models/staff.rb
+++ app/models/staff.rb
@@ -1,10 +1,18 @@
 class Staff < ApplicationRecord
+  incldue StringNormalizer
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :timeoutable
 
   has_many :staff_event_histories, dependent: :destroy
+
+  before_validation do
+    self.family_name = normalize_as_name(family_name)
+    self.given_name = normalize_as_name(given_name)
+    self.family_name_kana = normalize_as_furigana(family_name_kana)
+    self.given_name_kana = normalize_as_furigana(given_name_kana)
+  end
 
   KATAKANA_REGEXP = /\A[\p{katakana}\u{30fc}]+\z/
 
