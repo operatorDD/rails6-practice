49b6edc7faaa7fa727f369747ab4d257e425e64e
duho Kim <operatordd.kdh@gmail.com>
2020-07-01 23:58:57 +0900

    [update]emailバリデーション追加

--- app/models/staff.rb
+++ app/models/staff.rb
@@ -8,6 +8,7 @@ class Staff < ApplicationRecord
   has_many :staff_event_histories, dependent: :destroy
 
   before_validation do
+    self.email = normalize_as_email(email)
     self.family_name = normalize_as_name(family_name)
     self.given_name = normalize_as_name(given_name)
     self.family_name_kana = normalize_as_furigana(family_name_kana)
@@ -16,6 +17,7 @@ class Staff < ApplicationRecord
 
   KATAKANA_REGEXP = /\A[\p{katakana}\u{30fc}]+\z/.freeze
 
+  validates :email, presence: true, 'valid_email_2/email': true, uniqueness: { case_sensitive: false }
   validates :family_name, :given_name, presence: true
   validates :family_name_kana, :given_name_kana, presence: true,
                                                  format: { with: KATAKANA_REGEXP, allow_blank: true }
